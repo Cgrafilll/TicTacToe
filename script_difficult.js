4e4df9a68e6bdd48b2a3ebda4e926d1c74eeb5b5
Seeaannnn <152872212+Seeaannnn@users.noreply.github.com>
2024-02-19 18:01:17 +0800

    Update script_difficult.js

--- script_difficult.js
+++ script_difficult.js
@@ -206,29 +206,27 @@ function getBlockingMove() {
 function makeAIMove(cell) {
     aiIsThinking = true; // Set the flag to indicate AI is thinking
 
-    setTimeout(() => {
-        if (cell) {
-            cell.textContent = 'O'; // Make sure AI is always 'O'
-            Turns();
-            if (checkWinner()) {
-                highlightWinner();
-                setTimeout(() => {
-                    alert('Player X wins!');
-                    gameOver = true;
-                }, 800);
-            } else if (isBoardFull()) {
-                alert('It\'s a Draw!')
-                resetBoard();
+    if (cell) {
+        cell.textContent = 'O'; // Make sure AI is always 'O'
+        Turns();
+        if (checkWinner()) {
+            highlightWinner();
+            setTimeout(() => {
+                alert('Player X wins!');
                 gameOver = true;
-            }
+            }, 800);
+        } else if (isBoardFull()) {
+            alert('It\'s a Draw!')
+            resetBoard();
+            gameOver = true;
         }
+    }
 
-        currentPlayer = 'X'; // Switch back to player X after AI move
-        aiIsThinking = false; // Reset the flag after AI finishes its move
+    currentPlayer = 'X'; // Switch back to player X after AI move
+    aiIsThinking = false; // Reset the flag after AI finishes its move
 
-        // Re-enable player's ability to click after AI's turn
-        enablePlayerClick();
-    }, 1000);
+    // Re-enable player's ability to click after AI's turn
+    enablePlayerClick();
 }
 
 
